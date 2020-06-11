ceaf61961d0cdd0e3de26cc9d953187d645019a7
Elshan Dzhafarov (CryptoBadBoy) <raiman.elshan@gmail.com>
2020-06-11 21:38:11 +0300

    add ledger conf

--- pure-start.sh
+++ pure-start.sh
@@ -35,26 +35,10 @@ trim_by_one () {
     echo $1 | head -c -2 | tail -c +2
 }
 
-get_validator_template() {
-    printf '
-    {
-      "address": "%s",
-      "pub_key": {
-        "type": "tendermint/PubKeyEd25519",
-        "value": "%s"
-      },
-      "power": "10",
-      "name": ""
-    }
-    ' "$1" "$2"
-}
-
 rpc_urls=()
 p2p_urls=()
 configure_ledger_nodes () {
     # Building only once
-    pub_keys=()
-    address_list=()
     ledger_ids=()
     volume_list=()
 
@@ -80,6 +64,7 @@ configure_ledger_nodes () {
       docker build -f ledgernode.dockerfile \
         --build-arg ETH_NODE_URL=$1 \
         --build-arg WAVES_NODE_URL=$2 \
+        --build-arg VALIDATOR_INDEX=$i \
         -t "$image_name" .
 
       volume_name=$(printf "%s-volume-%s" $ledgernode_tag $i)
@@ -87,8 +72,6 @@ configure_ledger_nodes () {
 
       docker volume create "$volume_name"
     
-      # local ledger_id=$(docker run -d -v "$HOME/$volume_name:/proof-of-concept" "$image_name")
-      # local ledger_id=$(docker run -d -v "$HOME/$volume_name:/proof-of-concept" "$image_name")
       local ledger_id=$(
           docker run -d \
           --mount source=$volume_name,destination=$HOME/$volume_name "$image_name"
@@ -96,115 +79,14 @@ configure_ledger_nodes () {
 
       ledger_ids[i]=$ledger_id
       sleep 3
-
-      local priv_key=$(docker exec -it "$ledger_id" cat ./ledger-node/data/config/priv_validator_key.json)
-
-      pub_keys[i]=$(trim_by_one $(echo $priv_key | jq '.pub_key.value'))
-      address_list[i]=$(trim_by_one $(echo $priv_key | jq '.address'))
-    done
-
-    local validators='[]'
-
-    for ((j = 0; j<$ledgernodes_qty; j++))
-    do
-      local current_valid_obj=$(get_validator_template ${address_list[j]} ${pub_keys[j]})
-
-      local ledger_id=${ledger_ids[j]}
-      # update rpc & p2p urls
-      # cont_ip - just ip
-      local cont_ip=$(get_container_ip $ledger_id)
-      rpc_urls[j]="tcp://$cont_ip:$rpc_port"
-      p2p_urls[j]="tcp://$cont_ip:$p2p_port"
-
-      
-      if [[ j -gt 0 ]]
-      then
-        seeds_list+=','
-      fi
-
-      seed_address=$(echo ${p2p_urls[j]} | sed 's/tcp:\/\///')
-      seeds_list+="${address_list[j]}@$seed_address"
-
-      validators=$(echo $validators | jq ". + [$current_valid_obj]")
-    done
-
-    echo "Validator: $validators"
-    echo "Pub keys: ${pub_keys[@]}"
-    echo "Address list: ${address_list[@]}"
-    echo "RPC list: ${rpc_urls[@]}"
-    echo "P2P list: ${p2p_urls[@]}"
-
-    local config_dir_url='/ledger-node/data/config/'
-    local genesis_file_url='/ledger-node/data/config/genesis.json'
-    local toml_file_url='/ledger-node/data/config/config.toml'
-    for ((j = 0; j<$ledgernodes_qty; j++))
-    do
-      local ledger_id=${ledger_ids[j]}
-      
-      # update genesis.json
-      local template=$(echo ".validators = $(echo $validators)")
-
-      # docker cp ./test.txt f72bd9786d59:/proof-of-concept      
-
-      # docker exec "$ledger_id" cat "$genesis_file_url" | jq "$template" > "$genesis_file_url"
-      # new_genesis=$(docker exec "$ledger_id" cat "$genesis_file_url" | jq "$template")
-
-      # docker cp e09790e06ea4:/proof-of-concept/ledger-node/data/config/genesis.json I
-      
-      # get current genesis
-      docker cp "$ledger_id:/proof-of-concept$genesis_file_url" ./current.json
-      cat ./current.json | jq "$template" > current.json
-
-      docker cp ./current.json "$ledger_id:/proof-of-concept/$genesis_file_url"
-      docker exec "$ledger_id" cat ".$genesis_file_url"
-      # rm temp.json
-      
-      echo "Node #$((j+1)) genesis.json updated"
-      # docker exec -it "$ledger_id" cat "$genesis_file_url"
-      # sed 's/seeds\ =\ \"\"/seeds\ =\"like\"/' tendermint-template.toml 
-
-      # docker cp "$ledger_id:/proof-of-concept$toml_file_url" ./current.toml
-      # sed 's/seeds\ =\ \"\"/seeds\ =\"like\"/' tendermint-template.toml > ./current.toml
-
-      # echo "TOML" && cat ./current.toml
-      # docker cp ./current.toml "$ledger_id:/proof-of-concept/$toml_file_url"
-
-      # docker exec "$ledger_id" cat ".$toml_file_url"
-      rm current.json     
-
-      echo "Seeds: $seeds_list"
-      # docker stop "$ledger_id"
-      # docker start "$ledger_id"
-
-      # setting seeds for first node
-      if [[ j -eq 0 ]]
-      then
-         # sed 's/seeds\ =\ \"\"/seeds\ =\"dick\"/'
-         local sed_temp=$(printf 's/seeds\ =\ \"\"/seeds\ =\"%s\"/' "$seeds_list")
-         # docker exec "$ledger_id" sed "$sed_temp" "$toml_file_url" > "$toml_file_url"
-
-         docker cp "$ledger_id:/proof-of-concept$toml_file_url" ./current.toml
-
-         sed "$sed_temp" ./current.toml > ./config.toml
-         # sed 's/seeds\ =\ \"\"/seeds\ =\"like\"/' tendermint-template.toml > ./current.toml
-
-         echo "Apt toml" && cat ./config.toml
-         docker cp ./config.toml "$ledger_id:/proof-of-concept/$toml_file_url"
-         rm current.toml config.toml
-      fi
-
-      docker stop "$ledger_id"
     done
 
     # starting nodes from 1 to n 
-    for ((j = 1; j<$ledgernodes_qty; j++))
+    for ((j = 0; j<$ledgernodes_qty; j++))
     do
       local ledger_id=${ledger_ids[j]}
       docker start "$ledger_id"
     done
-   
-    # starting first node 
-    docker start "${ledger_ids[0]}"
 }
 
 pure_start () {
