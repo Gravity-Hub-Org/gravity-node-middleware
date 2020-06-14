34aafee1d9f9f0c900ac7e034527fcdc2b2d9e6f
CryptoBadBoy <raiman.elshan@gmail.com>
2020-06-14 23:20:56 +0300

    fix

--- pure-start.sh
+++ pure-start.sh
@@ -185,9 +185,10 @@ pure_start () {
     for ((j = 0; j<5; j++))
     do
         docker build -f ghnode-waves.dockerfile \
-         --build-arg NODE_URL="http://$waves_node_ip:6869" \
-         --build-arg LEDGER_URL="http://${rpc_urls[0]}" \
-         --build-arg KEY=$priv_keys_waves[$j] -t "$ghnode_waves_tag:$j" .
+        --build-arg NODE_URL="http://$waves_node_ip:6869" \
+        --build-arg LEDGER_URL="http://${rpc_urls[0]}" \
+        --build-arg KEY=KEY=${priv_keys_waves[j]} \
+        -t "$ghnode_waves_tag:$j" .
         
         docker run -d -p 26668:26657 \
          --mount source=$waves_gh_node_vol,destination=$HOME/$waves_gh_node_vol \
